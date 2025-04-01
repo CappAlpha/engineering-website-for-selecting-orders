744a8e2867178ed7090df5a103c5f48b0c4664fe
CappAlpha <egor.lapin.workspace@gmail.com>
2025-04-01 13:48:50 +0300

    Add seed

---
+++ prisma/seed.ts
@@ -0,0 +1,39 @@
+import { prisma } from "./prisma-client";
+import { hashSync } from 'bcrypt';
+
+async function up() {
+  await prisma.user.createMany({
+    data: [
+      {
+        fullName: 'User',
+        email: 'user@test.ru',
+        password: hashSync('user123', 10),
+        role: 'USER'
+      },
+      {
+        fullName: 'Admin',
+        email: 'admin@test.ru',
+        password: hashSync('admin123', 10),
+        role: 'ADMIN'
+      }
+    ]
+  })
+}
+
+async function down() {
+
+}
+
+async function main() {
+  await down();
+  await up();
+}
+
+main().then(async () => {
+  await prisma.$disconnect();
+}
+).catch(async (e) => {
+  console.error(e);
+  await prisma.$disconnect();
+  process.exit(1);
+})
