c912e4e9029cb66eca0e114ba4458f3ec6658489
CappAlpha <egor.lapin.workspace@gmail.com>
2025-04-17 01:24:20 +0300

    Style in product page and fetch

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,30 @@
+import { notFound } from "next/navigation";
+import { prisma } from "../../../prisma/prisma-client";
 import s from "./page.module.scss";
+import Image from "next/image";
 
-export default function ProductPage({
+export default async function ProductPage({
   params: { id },
 }: Readonly<{ params: { id: string } }>) {
-  return <div className={s.root}>Product {id}</div>;
+  const product = await prisma.product.findFirst({ where: { id: Number(id) } });
+
+  if (!product) {
+    return notFound();
+  }
+
+  const { imageUrl, name, description } = product;
+  return (
+    <div className={s.root}>
+      <div className={s.left}>
+        <Image className={s.img} src={imageUrl} alt={name} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          fill />
+      </div>
+      <div className={s.right}>
+        <ul>
+          <li>{name}</li>
+          <li>{description}</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
