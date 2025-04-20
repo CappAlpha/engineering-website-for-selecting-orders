033ea758f31b495d1efc53abb2f246cbccd359e7
CappAlpha <egor.lapin.workspace@gmail.com>
2025-04-20 15:57:24 +0300

    Some fixes

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,18 @@
 module.exports = {
   sortingMethod: "lineLength",
   plugins: ["./node_modules/prettier-plugin-sort-imports/dist/index.js"],
+  overrides: [
+    {
+      files: ["*.json", "*.md", "*.scss", "*.yml", "*.yaml"],
+      options: {
+        singleQuote: false,
+      },
+    },
+    {
+      files: ["*.ts", "*.tsx"],
+      options: {
+        arrowParens: "always",
+      },
+    },
+  ],
 };
