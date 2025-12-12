c3fed26c7608dfdd200ca34d5344189055614d57
CappAlpha <egor.lapin.workspace@gmail.com>
2025-12-12 12:31:01 +0300

    Fix prisma again

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,12 @@
+import { defineConfig, env } from "@prisma/config";
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "./prisma/schema.prisma",
-  datasource: { url: env("DB_DATABASE_URL") },
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: env("DB_DATABASE_URL"),
+  },
+  migrations: {
+    path: "prisma/migrations",
+  },
 });
--- prisma/prisma-client.ts
+++ prisma/prisma-client.ts
@@ -1,9 +1,8 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
+import "dotenv/config";
 
-const pool = new Pool({
-  connectionString: process.env.DB_POSTGRES_URL_NON_POOLING,
-});
-const adapter = new PrismaPg(pool);
+const connectionString = process.env.DB_DATABASE_URL;
+const adapter = new PrismaPg({ connectionString });
+
 export const prisma = new PrismaClient({ adapter });
